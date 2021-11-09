0e0acc72ab313d9b6791ac49bc44172fa2060d29
Noel Fernandez <noelfernandez98@gmail.com>
2021-11-09 10:08:04 +0000

    11/08 cc completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//write your code here
+function averageInArray(numbers){
+    let average = 0
+    for(let i = 0; i < numbers.length; i++){
+        average += numbers[i]
+    }
+    average = average/numbers.length
+    return average
+}
+
+
+console.log(averageInArray([2,4,5,7,8]))
+
+
+
+
+// averageInArray([2,4,5,7,8]) // returns 19.8
+// averageInArray([2,2,5,10]) // returns 4.75
+// averageInArray([2,2,2,2,2]) //returns 2
